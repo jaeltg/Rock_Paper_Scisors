31efc8758ffb362a473627b64cc4d596c5606eec
jaeltg <jaeltg@hotmail.com>
2021-03-13 13:24:26 +0000

    created a game class

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models.game import players, add_player, game_result, add_result, results
+from app.models.game import Game 
 from app.models.player import Player
 
 @app.route('/')
@@ -9,7 +9,8 @@ def welcome():
 
 @app.route('/play')
 def index():
-     return render_template('index.html', title="Play!", players=players, results=results)
+    game = Game("RPS")
+    return render_template('index.html', title="Play!", players=game.players, results=game.results)
 
 @app.route('/play', methods=['POST'])
 def create():
@@ -19,9 +20,10 @@ def create():
     player2_choice = request.form["choice2"]
     player1 = Player(player1_name, player1_choice)
     player2 = Player(player2_name, player2_choice)
-    add_player(player1)
-    add_player(player2)
-    result = game_result(player1, player2, player1.choice, player2.choice)
-    add_result(result)
+    game = Game("RPS")
+    game.add_player(player1)
+    game.add_player(player2)
+    result = game.game_result(player1, player2, player1.choice, player2.choice)
+    game.add_result(result)
     return redirect('/play')
     
--- app/models/game.py
+++ app/models/game.py
@@ -1,26 +1,29 @@
 from app.models.player import Player
 
-players = []
-results = []
+class Game:
+    def __init__(self, name, players = [], results = []):
+        self.name = name
+        self.players = players
+        self.results  = results
 
-def add_player(player):
-    players.append(player)
+    def add_player(self, player):
+        self.players.append(player)
 
-def add_result(result):
-    results.append(result)
+    def add_result(self, result):
+        self.results.append(result)
 
-def game_result(player1, player2, choice1, choice2):
-    if choice1.lower()  == "paper" and choice2.lower()  == "scissors":
-        return f"{player2.name} wins with SCISSORS!"
-    elif choice1.lower()  == "scissors" and choice2.lower()  == "paper":
-        return f"{player1.name} wins with SCISSORS!"
-    elif choice1.lower()  == "paper" and choice2.lower()  == "rock":
-        return f"{player1.name} wins with PAPER!"
-    elif choice1.lower()  == "rock" and choice2.lower()  == "paper":
-        return f"{player2.name} wins with PAPER!"
-    elif choice1.lower()  == "rock" and choice2.lower()  == "scissors":
-        return f"{player1.name} wins with ROCK!"
-    elif choice1.lower()  == "scissors" and choice2.lower()  == "rock":
-        return f"{player2.name} wins with ROCK!"   
-    elif choice1.lower()  == choice2.lower():
-        return f"It's a Tie!"   
+    def game_result(self, player1, player2, choice1, choice2):
+        if choice1.lower()  == "paper" and choice2.lower()  == "scissors":
+            return f"{player2.name} wins with SCISSORS!"
+        elif choice1.lower()  == "scissors" and choice2.lower()  == "paper":
+            return f"{player1.name} wins with SCISSORS!"
+        elif choice1.lower()  == "paper" and choice2.lower()  == "rock":
+            return f"{player1.name} wins with PAPER!"
+        elif choice1.lower()  == "rock" and choice2.lower()  == "paper":
+            return f"{player2.name} wins with PAPER!"
+        elif choice1.lower()  == "rock" and choice2.lower()  == "scissors":
+            return f"{player1.name} wins with ROCK!"
+        elif choice1.lower()  == "scissors" and choice2.lower()  == "rock":
+            return f"{player2.name} wins with ROCK!"   
+        elif choice1.lower()  == choice2.lower():
+            return f"It's a Tie!"   
